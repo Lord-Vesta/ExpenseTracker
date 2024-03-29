04bb2cef981d7e591826b559d7d9eab347df3b8e
Lord-Vesta <yashvardhan.jadhav@ellicium.com>
2024-03-29 15:06:14 +0530

    fix:fixed add feature and local storage also added add income feature with transaction display

--- script.js
+++ script.js
@@ -21,36 +21,94 @@ const balance = document.getElementById("balance");
 const TotalExpense = document.getElementById("TotalExpense");
 const TotalIncome = document.getElementById("TotalIncome");
 const AverageExpense = document.getElementById("AverageExpense");
+const addIncome = document.getElementById("AddIncome");
+const addExpenses = document.getElementById("AddExpenses");
+const dropdownCateogry = document.querySelectorAll(".category1");
 
-let data = [];
+let data = { expenseData: [], incomeData: [], transaction: [] };
 
-data = JSON.parse(localStorage.getItem("expenseData")) || [];
+data = JSON.parse(localStorage.getItem("Data")) || {
+  expenseData: [],
+  incomeData: [],
+  transaction: [],
+};
 
+// for modal display
 addbtn.addEventListener("click", () => {
   Modal.style.display = "block";
   mainBody.style.opacity = "0.5";
 });
 
+// to close modal
 closebtn.addEventListener("click", () => {
   Modal.style.display = "none";
   mainBody.style.opacity = "1";
+  addExpenses.style.backgroundColor = "white";
+  addExpenses.style.color = "#163a5f";
+  addIncome.style.backgroundColor = "white";
+  addIncome.style.color = "#163a5f";
+  addExpense.innerText = "Add";
 });
 
+// for modal display
 addbtn2.addEventListener("click", () => {
   Modal.style.display = "block";
   mainBody.style.opacity = "0.5";
 });
 
-addExpense.addEventListener("click", () => {
-  addData();
+// to add expenses
+addExpenses.addEventListener("click", () => {
+  console.log("inside addexpense");
+  addIncome.style.backgroundColor = "white";
+  addIncome.style.color = "#163a5f";
+  addExpenses.style.backgroundColor = "#163a5f";
+  addExpenses.style.color = "white";
+  addExpense.innerText = "Add Expense";
+  let i = 0;
+  addExpense.addEventListener("click", () => {
+    addData();
+    addExpenses.style.backgroundColor = "white";
+    addExpenses.style.color = "#163a5f";
+    addIncome.style.backgroundColor = "white";
+    addIncome.style.color = "#163a5f";
+    addExpense.innerText = "Add";
+  });
+});
+
+// to add income
+addIncome.addEventListener("click", () => {
+  addExpenses.style.backgroundColor = "white";
+  addExpenses.style.color = "#163a5f";
+  addIncome.style.backgroundColor = "#163a5f";
+  addIncome.style.color = "white";
+  addExpense.innerText = "Add Income";
+  addExpense.addEventListener("click", () => {
+    data.incomeData.push({
+      income: amount.value.trim(),
+      category: category.value.trim(),
+      description: description.value.trim(),
+      date: date.value.trim(),
+    });
+    localStorage.setItem("Data", JSON.stringify(data));
+    description.value = "";
+    category.value = "";
+    amount.value = "";
+    date.value = "";
+    Modal.style.display = "none";
+    mainBody.style.opacity = "1";
+    // console.log(income);
+    ExpenseOverview();
+    showtransaction();
+  });
 });
 
 function addData() {
+  console.log("inside add btn");
   let descriptionValue = description.value.trim();
   let categoryValue = category.value.trim();
   let amountValue = amount.value.trim();
   let dateValue = date.value.trim();
-
+  // console.log(descriptionValue);
   if (descriptionValue === "") {
     alert("add description value");
   }
@@ -63,28 +121,31 @@ function addData() {
   if (dateValue === "") {
     alert("add date");
   } else {
-    data.push({
-      id: data.length + 1,
+    console.log(data.expenseData);
+    data.expenseData.push({
+      id: data.expenseData.length + 1,
       description: descriptionValue,
       category: categoryValue,
       amount: amountValue,
       date: dateValue,
     });
 
-    localStorage.setItem("expenseData", JSON.stringify(data));
+    localStorage.setItem("Data", JSON.stringify(data));
     listData();
+    console.log(data);
     description.value = "";
     category.value = "";
     amount.value = "";
     date.value = "";
     Modal.style.display = "none";
     mainBody.style.opacity = "1";
+    ExpenseOverview();
   }
 }
 
 function listData() {
   let list = "";
-  data.forEach((item, index) => {
+  data.expenseData.forEach((item, index) => {
     list += `
         <tr >
                     <td>${item.amount}</td>
@@ -103,18 +164,18 @@ function listData() {
                     </div>
                   </div>
                     </td>
-                    
+
                   </tr>
         `;
   });
   expenseConatiner.innerHTML = list;
-  localStorage.setItem("expenseData", JSON.stringify(data));
-  ExpenseOverview();
+  localStorage.setItem("Data", JSON.stringify(data));
+  showtransaction();
 }
 
 function deleteItem(index) {
-  data.splice(index, 1);
-  localStorage.setItem("expenseData", JSON.stringify(data));
+  data.expenseData.splice(index, 1);
+  localStorage.setItem("Data", JSON.stringify(data));
   listData();
 }
 
@@ -122,19 +183,18 @@ function EditItem(index) {
   EditModal.style.display = "flex";
   mainBody.style.opacity = "0.5";
 
-  EditModalAmount.value = data[index].amount;
-  EditModalCategory.value = data[index].category;
-  EditModalDate.value = data[index].date;
-  EditModalDescription.value = data[index].description;
+  EditModalAmount.value = data.expenseData[index].amount;
+  EditModalCategory.value = data.expenseData[index].category;
+  EditModalDate.value = data.expenseData[index].date;
+  EditModalDescription.value = data.expenseData[index].description;
 
   function hanleEditDone() {
-    data[index].description = EditModalDescription.value.trim();
-    data[index].category = EditModalCategory.value.trim();
-    data[index].amount = EditModalAmount.value.trim();
-    data[index].date = EditModalDate.value.trim();
+    data.expenseData[index].description = EditModalDescription.value.trim();
+    data.expenseData[index].category = EditModalCategory.value.trim();
+    data.expenseData[index].amount = EditModalAmount.value.trim();
+    data.expenseData[index].date = EditModalDate.value.trim();
 
-    localStorage.setItem("expenseData", JSON.stringify(data));
-    console.log(data);
+    localStorage.setItem("Data", JSON.stringify(data));
     listData();
     EditModal.style.display = "none";
     mainBody.style.opacity = "1";
@@ -146,13 +206,37 @@ function EditItem(index) {
 function ExpenseOverview() {
   let sum = 0;
   let avg = 0;
-  let n = data.length;
-  data.forEach((item, index) => {
+  let inc = 0;
+  let balanceamt = 0;
+  let n = data.expenseData.length;
+  data.expenseData.forEach((item) => {
     sum += parseFloat(item.amount);
     avg = sum / n;
   });
-  TotalExpense.innerText = `$${sum.toFixed(2)}`;
-  AverageExpense.innerText = `$${avg.toFixed(2)}`
+  data.incomeData.forEach((item) => {
+    inc += parseFloat(item.income);
+  });
+  balanceamt = parseFloat(inc - sum);
+
+  data.transaction.push({
+    totalExpense: sum.toFixed(2),
+    averageExpense: avg.toFixed(2),
+    totalincome: inc.toFixed(2),
+    balance: balanceamt.toFixed(2),
+  });
+  localStorage.setItem("data", JSON.stringify(data));
+
+  // console.log(data.transaction);
+}
+
+function showtransaction() {
+  data.transaction.forEach((list) => {
+    TotalExpense.innerText = `$${list.totalExpense}`;
+    AverageExpense.innerText = `$${list.averageExpense}`;
+    TotalIncome.innerText = `$${list.totalincome}`;
+    balance.innerText = `$${list.balance}`;
+  });
 }
 
 listData();
+showtransaction();
